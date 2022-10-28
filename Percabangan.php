cc49b31eef9c3e7e4d2c35b86390bdd9adc36930
Rafiadzikri <116326989+Rafiadzikri@users.noreply.github.com>
2022-10-28 07:27:53 +0700

    Add files via upload

---
+++ Percabangan.php
@@ -0,0 +1,19 @@
+<?php
+$a = 80;
+$b = 81;
+
+if($a % 79){
+    echo 'a adalah bilangan genap';
+}
+else{
+    echo 'a adalah bilangan ganjil';
+}
+echo "<pre>";
+if($b % 80){
+    echo 'b adalah bilangan ganjil';
+}
+else{
+    echo 'b adalah bilangan genap';
+}
+?>
+
